b95f86fe5f41e28dd26afffe3698140688cbb6e9
RocioValentin <rociovcl.10@gmail.com>
2021-01-19 11:48:38 -0500

    Funcionalidad en los botones

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import cipher from './cipher.js';
 
+//declarar variables
+//const num = document.getElementById("offset").nodeValue;
+//const str = document.getElementById("inputclave").nodeValue;
+
 console.log(cipher);
-console.log(cipher.Encode(2,"H"));
-console.log(cipher.Decode(2,"H"));
+
+//funcionalidad a los botones
+
+document.getElementById('Cifrar').onclick = function (){
+    //cipher.Encode(2,"H");
+    console.log(cipher.Encode(2,"H"));
+}
+
+document.getElementById('Descifrar').onclick = function (){
+    console.log(cipher.Decode(2,"H"));
+}
