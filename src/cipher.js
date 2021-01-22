dc4c7256e849bd2e2eebaff0d71be3341535720b
RocioValentin <rociovcl.10@gmail.com>
2021-01-21 19:44:08 -0500

    Pasando Test TypeError

--- src/cipher.js
+++ src/cipher.js
@@ -1,34 +1,48 @@
 const cipher = {
-  
-
   //...
 };
 
-//let num;
-//let str;
-
 //creando la funcion Encode
-cipher.Encode = function(num,str){
+cipher.encode = function(num,str){
+
+  if (typeof num !== 'number' || isNaN(num) || num ==='' || typeof str !== 'string'){
+    throw new TypeError("Invalido")
+  }
 
   let newStr = "";
   //bucle for en el str
   for (let i = 0; i < str.length; i++){
 
+  //variables para realizar la operacion
   var asciiNum = str[i].charCodeAt();
   var asciiNew = (asciiNum - 65 + num)%26 + 65;
   
+  //resultado concatenado
   newStr += String.fromCharCode(asciiNew);
 }
 return newStr;
 }
 
-cipher.Decode = function(num,str){
+cipher.decode = function(num,str){
+
+  if (typeof num !== 'number' || isNaN(num) || num ==='' || typeof str !== 'string'){
+    throw new TypeError ("invalido")
+  }
+
   let newStr1 = "";
   for (let i = 0; i < str.length; i++){
-
+    
+    //variables para realizar la operacion
     var asciiNum1 = str[i].charCodeAt();
-    var asciiNew1 = (asciiNum1 - 65 - num)%26 + 65;
-
+    var letra = asciiNum1 - 65;
+    var asciiNew1;
+    if (num > letra){
+      asciiNew1 = (25 -((num-1) - letra)%26) + 65;
+    }
+    else{
+      asciiNew1 = (asciiNum1 - 65 - num)%26 + 65;
+    }
+    
     newStr1 += String.fromCharCode(asciiNew1); 
   }
   return newStr1;
--- src/index.js
+++ src/index.js
@@ -5,24 +5,36 @@ let num;
 let str;
 
 
-
-console.log(cipher);
+//console.log(cipher);
 
 //funcionalidad a los botones
 
 document.getElementById("Cifrar").onclick = function () {
     //cipher.Encode(2,"H");
     //console.log(cipher.Encode(num,str));
+    //llamar variables de ingreso dentro de la funcion
     num = parseInt(document.getElementById("offset").value);
     str = document.getElementById("inputclave").value;
-    document.getElementById("outputclave").innerHTML = (cipher.Encode(num, str));
-    console.log(num);
-    console.log(str);
+
+    try {
+        document.getElementById("outputclave").innerHTML = (cipher.encode(num, str));
+    } catch(error){
+        document.getElementById("outputclave").innerHTML = error;
+    }
+    
+    //console.log(num);
+    //console.log(str);
 }
+
 
 document.getElementById("Descifrar").onclick = function () {
     //console.log(cipher.Decode(num,str));
     num = parseInt(document.getElementById("offset").value);
     str = document.getElementById("inputclave").value;
-    document.getElementById("outputclave").innerHTML = (cipher.Decode(num, str));
+    try {
+        document.getElementById("outputclave").innerHTML = (cipher.decode(num, str));
+    } catch(error){
+        document.getElementById("outputclave").innerHTML = error;
+    }
+    
 }
